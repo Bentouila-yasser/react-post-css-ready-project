f94c80498996ba06fc82efb332f5a1fcfeb49e9b
Bentouila-yasser <ben0yasser0touila@gmail.com>
2022-01-31 18:53:47 +0100

    Initial setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,44 +2,8 @@ import React from 'react';
 
 export default function App() {
   return (
-    <div className='fullcontainer'>
-      <div className='container'>
-        <nav>
-          <div className='logo'>
-            <a href='#'>
-              <img src='./img/logo.PNG' alt='logo' />
-            </a>
-          </div>
-          <ul className='left'>
-            <li>
-              <a href='#'>Product</a>
-            </li>
-            <li>
-              <a href='#'>Constomers</a>
-            </li>
-            <li>
-              <a href='#'>Community</a>
-            </li>
-            <li>
-              <a href='#'>Pricing</a>
-            </li>
-          </ul>
-          <ul className='right'>
-            <li>
-              <a href='#'>Docs</a>
-            </li>
-            <li>
-              <a href='#'>Support</a>
-            </li>
-            <li>
-              <a href='#'>Login</a>
-            </li>
-            <li>
-              <a href='#'>Sign Up</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <div className='wrapper'>
+      <p className='paragraph'>say h</p>
     </div>
   );
 }
